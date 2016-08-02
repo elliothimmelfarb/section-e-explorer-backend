74b381d0c33d509e34b8ff26fe74d9a1e1afd4d3
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-08-02 09:53:55 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const PORT = process.env.PORT || 8000;
+
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+const server = http.createServer(app);
+
+// const mongoose = require('mongoose');
+// const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/final-test-yelp';
+// mongoose.connect(mongoUrl, err => {
+//   console.log(err || `MongoDB connected to ${mongoUrl}`); // eslint-disable-line no-console
+// });
+
+server.listen(PORT, err => {
+  console.log(err || `Server listening on port ${PORT}`); // eslint-disable-line no-console
+});
+
+// view engine setup
+
+
+// uncomment after placing your favicon in /public
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// /////// ROUTERS //////////////
+
+app.use('/api', require('./routes/api.routes'));
+
+// //////////////////////////////
+
+app.get('/', (req, res) => {
+  res.send("Hello");
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.send(err);
+});
